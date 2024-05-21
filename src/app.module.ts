369e950a367fd78af68de63184bb8f048afb4511
Luis Angel Rodriguez <darkangel_10000@hotmail.com>
2024-05-20 22:47:20 -0600

    s3 heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -60,12 +60,11 @@ import { Afiliado } from './afiliados/entities/afiliado.entity';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
+        url: configService.get('DATABASE_URL'),
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
         entities: [
           Evento,
           User,
